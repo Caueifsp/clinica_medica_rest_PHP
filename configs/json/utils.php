4e63e1d8e59e48de69e646aea1382362f9935d39
Cauê Gastaldi <82604475+Caueifsp@users.noreply.github.com>
2022-06-08 12:11:09 -0300

    Add files via upload

---
+++ configs/json/utils.php
@@ -0,0 +1,47 @@
+<?php
+
+/* 
+    Exemplo:
+    parametroValidos($_POST, ["id", "nome"]);
+*/
+function parametrosValidos($metodo, $lista)
+{
+    $obtidos = array_keys($metodo);
+    $nao_encontrados = array_diff($lista, $obtidos);
+    if (empty($nao_encontrados)) {
+        foreach ($lista as $p) {
+            if (empty(trim($metodo[$p])) and trim($metodo[$p]) != "0") {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+/* 
+    Exemplo:
+    isMetodo("PUT");
+*/
+function isMetodo($metodo)
+{
+    if (!strcasecmp($_SERVER['REQUEST_METHOD'], $metodo)) {
+        return true;
+    }
+    return false;
+}
+
+function filterIsInt($v) {
+    return filter_var($v, FILTER_VALIDATE_INT);
+}
+
+function filterIsEmail($v) {
+    return filter_var($v, FILTER_VALIDATE_EMAIL);
+}
+
+function emptyString($str) {
+    if(strlen(trim($str)) == 0) {
+        return true;
+    } 
+    return false;
+}
