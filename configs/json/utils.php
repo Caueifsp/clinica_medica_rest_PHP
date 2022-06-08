2480c9dd5d35a11207eb7b32dbce14d20ed5ada3
Cauê Gastaldi <82604475+Caueifsp@users.noreply.github.com>
2022-06-08 12:17:46 -0300

    Add files via upload

--- configs/json/utils.php
+++ configs/json/utils.php
@@ -31,6 +31,7 @@ function isMetodo($metodo)
     return false;
 }
 
+
 function filterIsInt($v) {
     return filter_var($v, FILTER_VALIDATE_INT);
 }
@@ -44,4 +45,66 @@ function emptyString($str) {
         return true;
     } 
     return false;
+}
+
+function adminLoginIsUnique($login, $idAdmin = null) {
+    require_once("./classes/Administrador.php");
+    try {
+        $conexao = Conexao::getConexao();
+        $stmt = $conexao->prepare("SELECT id, COUNT(*) AS qtde from administradores WHERE login = ?");
+        $stmt->execute([$login]);
+
+        $qtde = $stmt->fetchAll()[0]["qtde"];
+        $id = $stmt->fetchAll()[0]["id"];
+        if ($qtde == 0 || qtde > 0 and isset($idAdmin) and $id == $idAdmin)
+            return true;;
+        return false;
+    } catch (Exception $e) {
+        header("HTTP/1.1 500 Internal Server Error");
+        echo json_encode([
+            "msg" => "Houve um erro na base de dados: " . $e->getMessage()
+        ]);
+        exit;
+    }
+}
+
+function pacienteLoginIsUnique($login, $idPaciente = null) {
+    require_once("./classes/Paciente.php");
+    try {
+        $conexao = Conexao::getConexao();
+        $stmt = $conexao->prepare("SELECT id, COUNT(*) AS qtde from pacientes WHERE login = ?");
+        $stmt->execute([$login]);
+
+        $qtde = $stmt->fetchAll()[0]["qtde"];
+        $id = $stmt->fetchAll()[0]["id"];
+        if ($qtde == 0 || qtde > 0 and isset($idPaciente) and $id == $idPaciente)
+            return true;;
+        return false;
+    } catch (Exception $e) {
+        header("HTTP/1.1 500 Internal Server Error");
+        echo json_encode([
+            "msg" => "Houve um erro na base de dados: " . $e->getMessage()
+        ]);
+        exit;
+    }
+}
+
+function dataEhValida($data, $formato) {
+
+    try {
+        $dateTime = DateTime::createFromFormat($formato, $data);
+        $dia = $dateTime->format("d");
+        $mes = $dateTime->format("m");
+        $ano = $dateTime->format("Y");
+
+        if (!checkdate($mes, $dia, $ano))
+            return false;
+
+        return true;
+    } catch (Exception $e) {
+        return false;
+    }
+    /*if (!DateTime::createFromFormat("Y-m-d", $data))
+        return false;
+    return true;*/
 }
